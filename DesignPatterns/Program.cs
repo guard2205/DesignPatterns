227b6680dad5a340a3ef2a6d804a1a0140ca1221
guard2205 <gctiberio@gmail.com>
2019-03-12 21:41:25 +0100

    Solid Open Close Principle

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using DesignPatterns.Solid;
 namespace DesignPatterns
 {
@@ -11,9 +12,38 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
             var j = new Journal();
+            Console.WriteLine("SolidSRP");
             j.AddEntry("I wake up");
             j.AddEntry("I lunched");
             Console.WriteLine(j.ToString());
+            Console.WriteLine("===========\n SolidORP");
+            Console.WriteLine("*Filtering Without SOLIDORP*");
+            var apple = new Product("Appel", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+            var pf = new ProductFilter();
+
+            Console.WriteLine("Green products (old): ");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            Console.WriteLine("*Filtering With SOLIDORP*");
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new): ");
+            foreach( var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+            Console.WriteLine("Large blue items");
+            foreach(var p in bf.Filter(products, new AndSpecification<Product>( new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large))))
+            {
+                Console.WriteLine($" - {p.Name} is blue and big");
+            }
+            Console.WriteLine("===========\n SolidORP");
 
             Console.WriteLine("Enter a key");
             Console.ReadKey();
--- DesignPatterns/Solid/SolidOCP.cs
+++ DesignPatterns/Solid/SolidOCP.cs
@@ -7,11 +7,107 @@ using System.Threading.Tasks;
 namespace DesignPatterns.Solid
 {
     /// <summary>
+    /// Open-Closed Principle 
     /// Entities should be open for extension 
     /// but closed for modification
     /// </summary>
-    public class SolidOCP
-    {
 
+    // Without Using SolidOCP
+    public enum Color
+    { Red,Green,Blue}
+    public enum Size
+    { Small,Medium,Large}
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+        public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        }
+    }
+
+
+    // Without Using SolidOCP
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color; 
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color; 
+        }
+    }
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size)
+        { this.size = size; }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
     }
 }
